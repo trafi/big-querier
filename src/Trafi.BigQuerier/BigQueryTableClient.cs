741d1e4087bd979a2abea8aff7b5fe5964ef0023
Nerijus Arlauskas <nercury@gmail.com>
2017-09-15 13:59:17 +0300

    Slap licence on everything.

--- src/Trafi.BigQuerier/BigQueryTableClient.cs
+++ src/Trafi.BigQuerier/BigQueryTableClient.cs
@@ -1,4 +1,11 @@
-﻿using Google.Cloud.BigQuery.V2;
+﻿// Copyright 2017 TRAFI
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+using Google.Cloud.BigQuery.V2;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
--- src/Trafi.BigQuerier/Mapper/Record.cs
+++ src/Trafi.BigQuerier/Mapper/Record.cs
@@ -1,4 +1,11 @@
-﻿using Google.Apis.Bigquery.v2.Data;
+﻿// Copyright 2017 TRAFI
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+using Google.Apis.Bigquery.v2.Data;
 using Google.Cloud.BigQuery.V2;
 using System;
 using System.Collections.Generic;
